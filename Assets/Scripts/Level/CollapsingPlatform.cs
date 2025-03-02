0d650e0cef56e8247db7cc34ebc696f929a3748b
MarvinKnobloch <122169416+MarvinKnobloch@users.noreply.github.com>
2025-03-02 18:08:34 +0100

    CollapsingPlatform update

--- Assets/Scripts/Level/CollapsingPlatform.cs
+++ Assets/Scripts/Level/CollapsingPlatform.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 public class CollapsingPlatform : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-    private bool collapsing;
     private SpriteRenderer spriteRenderer;
+    private GameObject childObj;
+
+    private bool collapsing;
     private int changeSpeedAmount = 3;
     private int speedChanges;
 
     [SerializeField] private float collapseTime;
     private float timer;
-    [SerializeField] private float blinkSpeed;
+    [SerializeField] private float respawnTime;
+    [SerializeField] private float baseBlinkSpeed;
+    private float blinkSpeed;
     private float blinkTimer;
     private bool normalColor;
     private Color baseColor;
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
-        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        childObj = transform.GetChild(0).gameObject;
+        spriteRenderer = childObj.GetComponent<SpriteRenderer>();
         float xsize = spriteRenderer.size.x;
         boxCollider.size = new Vector2(xsize * 0.98f, transform.GetChild(0).transform.localScale.y);  // 0.49f
-        transform.GetChild(0).GetComponent<BoxCollider2D>().size = new Vector2(xsize, 1);
+        childObj.GetComponent<BoxCollider2D>().size = new Vector2(xsize, 1);
 
         baseColor = spriteRenderer.color;
     }
@@ -38,6 +43,7 @@ public class CollapsingPlatform : MonoBehaviour
     }
     IEnumerator StartCollapsing()
     {
+        speedChanges = 0;
         collapsing = true;
         while (speedChanges < changeSpeedAmount)
         {
@@ -46,10 +52,16 @@ public class CollapsingPlatform : MonoBehaviour
             speedChanges++;
         }
         StopAllCoroutines();
-        Destroy(gameObject);
+        spriteRenderer.color = baseColor;
+        normalColor = false;
+        childObj.SetActive(false);
+        boxCollider.enabled = false;
+        StartCoroutine(Respawn());
     }
     IEnumerator PlatformBlink()
     {
+        blinkSpeed = baseBlinkSpeed;
+        blinkTimer = 0;
         while (true)
         {
             blinkTimer += Time.deltaTime;
@@ -62,5 +74,12 @@ public class CollapsingPlatform : MonoBehaviour
             }
             yield return null;
         }
+    }
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        collapsing = false;
+        boxCollider.enabled = true;
+        childObj.SetActive(true);
     }
 }
