8681cb469c77332dbb6d26ee42a95ce72adbf3df
MarvinKnobloch <122169416+MarvinKnobloch@users.noreply.github.com>
2025-03-09 23:21:34 +0100

    Gate Update

--- Assets/Scripts/Level/MoveOnInteraction.cs
+++ Assets/Scripts/Level/MoveOnInteraction.cs
@@ -5,6 +5,7 @@ public class MoveOnInteraction : MonoBehaviour, IActivate
 {
     private Vector3 startPosi;
     private Vector3 endPosi;
+    private SpriteRenderer spriteRenderer;
 
     private int currentGoals;
     public int requiredGoals;
@@ -19,6 +20,8 @@ public class MoveOnInteraction : MonoBehaviour, IActivate
 
     [Header("SpriteUpdate")]
     [SerializeField] private bool spriteUpdate;
+    private int currentSpriteNumber;
+    [SerializeField] private Sprite[] gateSprites;
 
     private State state;
     public enum State
@@ -31,6 +34,7 @@ public class MoveOnInteraction : MonoBehaviour, IActivate
     {
         startPosi = transform.position;
         endPosi = transform.GetChild(1).gameObject.transform.position;
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
     private void FixedUpdate()
     {
@@ -59,6 +63,11 @@ public class MoveOnInteraction : MonoBehaviour, IActivate
     public void Activate()
     {
         currentGoals++;
+        if(isactiv == false && spriteUpdate)
+        {
+            currentSpriteNumber++;
+            spriteRenderer.sprite = gateSprites[currentSpriteNumber];
+        }
 
         CheckRequirements();
     }
@@ -100,6 +109,12 @@ public class MoveOnInteraction : MonoBehaviour, IActivate
             state = State.moveToStart;
         }
         currentGoals--;
+
+        if(isactiv == false && currentGoals >= 0 && spriteUpdate)
+        {
+            currentSpriteNumber--;
+            spriteRenderer.sprite = gateSprites[currentSpriteNumber];
+        }
     }
     private void Move(Vector3 start, Vector3 end)
     {
