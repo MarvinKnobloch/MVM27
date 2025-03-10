80bbd5d06c416388fd5a8c98171c03a0a51f4ad6
RoyalPizza <68163537+RoyalPizza@users.noreply.github.com>
2025-03-10 15:08:40 -0400

    Roller Updates

--- Assets/Scenes/Pizza/Roller Enemy/README.md
+++ Assets/Scenes/Pizza/Roller Enemy/README.md
@@ -2,13 +2,24 @@
 The roller basically rolls along the ground from its start point to a waypoint.
 The roller cannot go up/down, only left/right.
 
+## Gizmos
+- red line is the vision line. It looks for the player in that line.
+- Freeze On Hit Time is basically when the roller stops moving for a brief moment after getting hit
+- Waypoint is the position the roller will move to. Once it reaches it, it will go back to start.
+
 ## Combat
 - Moves left/right along from start point to waypoint
 - Raycasts forward to check for player
 - If player is detected, it will speed up towards the player and ram them
-- If player is hit, it will push the player back and run through them
+- If player is hit
+    - It temporarily turns off collision so that it can "go through" the player
     - A configurable damage timer will start, that prevents damaging the player again
 - If player is not hit, it will return to normal speed
 
 ## Visuals
 - We have Idle, Hit, and Die animations.
+
+## Notes
+- How to handle situations where dev put waypiont in position roller cannot get to?
+    - For now, the roller will simply just spin forever in the direction its trying to go.
+- I think it would be fun to add "run away speed". Something where if the player hits the roller, they try to speed away.
--- Assets/Scripts/Enemies/Roller/RollerEnemy.cs
+++ Assets/Scripts/Enemies/Roller/RollerEnemy.cs
@@ -1,29 +1,37 @@
 using System;
 using UnityEngine;
 
-// TODO: Either implement gravity, or switch to not kinmatic
-
 public class RollerEnemy : MonoBehaviour
 {
     [Header("Components")]
+    [SerializeField] private Collider2D col;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator animator;
     [SerializeField] private Health healthComponent;
 
     [Header("Config")]
     [SerializeField, Min(0f)] private float speed = 2f;
+    [Tooltip("The speed we move if the target is in sight.")]
+    [SerializeField, Min(0f)] private float chaseSpeed = 5f;
     [Tooltip("The waypoint to walk to and from.")]
     [SerializeField] private Transform waypoint;
     [Tooltip("The damage dealt to the player if they collide")]
     [SerializeField, Min(0)] private int damage = 1;
     [Tooltip("The time to freeze the enemy on hit. Set to 0 to disable.")]
     [SerializeField, Min(0f)] private float freezeOnHitTime = .3f;
+    [SerializeField, Min(0f)] private float visionRange = 8f;
+    [SerializeField] private LayerMask visionMask;
+    [Tooltip("The time in seconds we are to wait before allowing a hit on the target again.")]
+    [SerializeField, Min(0f)] private float attackBuffer = 1f;
 
+    private Transform target;
     private Vector2 startPosition = Vector2.zero;
     private Vector2 moveDirection = Vector2.zero;
     private bool movingTowardsWaypoint = true;
     private float hitTime = 0f;
     private bool dead = false;
+    private bool targetInSight = false;
+    private float lastTimeHitPlayer = 0f; // this the time we hit the player, not the player attacking us
 
     private const float WAYPOINT_PROXIMITY = 0.1f;
     private const float DEATH_DESTROY_TIME = 0.5f;
@@ -34,6 +42,8 @@ public class RollerEnemy : MonoBehaviour
 
     private void Awake()
     {
+        if (col == null)
+            throw new ArgumentNullException(nameof(col));
         if (rb == null)
             throw new ArgumentNullException(nameof(rb));
         if (healthComponent == null)
@@ -50,15 +60,28 @@ public class RollerEnemy : MonoBehaviour
     private void Start()
     {
         startPosition = rb.position;
+        target = Player.Instance.transform;
     }
 
     private void Update()
     {
         if (moveDirection != Vector2.zero)
             UpdateSpriteDirection(moveDirection);
+        
+        if (moveDirection != Vector2.zero)
+        {
+            var visionHit = Physics2D.Raycast(rb.position, moveDirection, visionRange, visionMask);
+            targetInSight = visionHit.collider != null && visionHit.collider.CompareTag("Player");
+        }
 
         if (hitTime != 0f && Time.time - hitTime > freezeOnHitTime)
             hitTime = 0f;
+
+        if (lastTimeHitPlayer > 0f && Time.time - lastTimeHitPlayer > attackBuffer)
+        {
+            lastTimeHitPlayer = 0f;
+            col.excludeLayers = 0;
+        }
     }
 
     private void FixedUpdate()
@@ -74,23 +97,29 @@ public class RollerEnemy : MonoBehaviour
         }
 
         moveDirection = (targetPosition - rb.position).normalized;
-        moveDirection.y = 0f; // dont allow vertical movement
+        moveDirection.y = 0f;
 
-        rb.MovePosition(rb.position + (moveDirection * speed * Time.fixedDeltaTime));
+        float movementSpeed = (targetInSight || lastTimeHitPlayer != 0f) ? chaseSpeed : speed;
+
+        rb.linearVelocity = new Vector2(moveDirection.x * movementSpeed, rb.linearVelocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (dead)
+        if (dead || lastTimeHitPlayer > 0f)
             return;
-
-        if (collision.gameObject.CompareTag("Player"))
+        
+        if (collision.gameObject.CompareTag(target.tag))
+        {
             Player.Instance.health.TakeDamage(damage, false);
+            lastTimeHitPlayer = Time.time;
+            col.excludeLayers |= 1 << target.gameObject.layer;
+        }
     }
 
-    private bool NearPosition(Vector2 targePosition)
+    private bool NearPosition(Vector2 targetPosition)
     {
-        return (Mathf.Abs(rb.position.x - targePosition.x) < WAYPOINT_PROXIMITY);
+        return (Mathf.Abs(rb.position.x - targetPosition.x) < WAYPOINT_PROXIMITY);
     }
 
     private void UpdateSpriteDirection(Vector3 direction)
@@ -101,16 +130,29 @@ public class RollerEnemy : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 180, 0);
     }
 
+    // this is triggered from the health component
     private void OnHit()
     {
         hitTime = Time.time;
         animator.Play(HIT_ANIM);
     }
 
+    // this is triggered from the health component
     private void OnDie()
     {
         dead = true;
         animator.Play(DIE_ANIM);
         Destroy(gameObject, DEATH_DESTROY_TIME);
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector2 direction = moveDirection != Vector2.zero ? moveDirection : Vector2.right;
+        Gizmos.DrawLine(rb.position, rb.position + direction * visionRange);
+
+        UnityEditor.Handles.Label(rb.position + Vector2.up * 0.5f, targetInSight ? "Target in Sight" : "No Target");
+    }
+#endif
 }
