5c47c0bb92ae72d5513e03885f08f8cc7d3c15ee
MarvinKnobloch <122169416+MarvinKnobloch@users.noreply.github.com>
2025-03-14 17:35:55 +0100

    Upgrades + Boss projectiles +level fixes +other fixes

--- Assets/Scripts/Level/OverworldUpgrade.cs
+++ Assets/Scripts/Level/OverworldUpgrade.cs
@@ -1,29 +1,56 @@
+using System;
 using UnityEngine;
 
-public class OverworldUpgrade : MonoBehaviour
+public class OverworldUpgrade : MonoBehaviour, IInteractables
 {
     [SerializeField] private Upgrades.StatsUpgrades stat;
     [SerializeField] private int amount;
     [SerializeField] private int ID;
     [TextArea][SerializeField] private string upgradeText;
 
+    private Animator animator;
+    private CircleCollider2D circleCollider;
+
+    [Space]
+    [SerializeField] private string actionText;
+    public GameObject interactObj { get => gameObject; }
+    public string interactiontext => actionText;
+
     private void Awake()
     {
+        animator = GetComponent<Animator>();
+        circleCollider = GetComponent<CircleCollider2D>();
         if(PlayerPrefs.GetInt("Upgrade" + ID) == 1)
         {
-            gameObject.SetActive(false);
+            Collected();
         }
+    }
+    public void Interaction()
+    {
+        Player.Instance.AddStatUpgrade(stat, amount);
+        PlayerPrefs.SetInt("Upgrade" + ID, 1);
+
+        GameManager.Instance.playerUI.MessageBoxEnable(upgradeText + amount + ".");
+
+        Collected();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Player.Instance.AddStatUpgrade(stat, amount);
-            PlayerPrefs.SetInt("Upgrade" + ID, 1);
-
-            GameManager.Instance.playerUI.MessageBoxEnable(upgradeText + amount + ".");
-
-            gameObject.SetActive(false);
+            Player.Instance.playerInteraction.AddInteraction(this);
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player.Instance.playerInteraction.RemoveInteraction(this);
+        }
+    }
+    private void Collected()
+    {
+        circleCollider.enabled = false;
+        animator.enabled = true;
     }
 }
