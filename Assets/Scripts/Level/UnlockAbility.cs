e7ea63a20abf5ce5df7f28b5b43671b2167707b1
MarvinKnobloch <122169416+MarvinKnobloch@users.noreply.github.com>
2025-03-18 12:30:25 +0100

    Shrine dialog

--- Assets/Scripts/Level/UnlockAbility.cs
+++ Assets/Scripts/Level/UnlockAbility.cs
@@ -8,17 +8,28 @@ public class UnlockAbility : MonoBehaviour, IInteractables
     public string interactiontext => actionText;
 
     [SerializeField] private GameManager.AbilityStrings abilityString;
+    [SerializeField] private DialogObj dialog;
     [TextArea]
     [SerializeField] private string unlockText;
     [SerializeField] private bool disableOnCollect;
 
+    [Header("ShrineUnique")]
+    [SerializeField] private VoidEventChannel shrineEndEvent;
     [SerializeField] private MoveOnInteraction objAfterCollect;
 
+
+    private void OnEnable()
+    {
+        shrineEndEvent.OnEventRaised += ActivateMusic;
+    }
+    private void OnDisable()
+    {
+        shrineEndEvent.OnEventRaised += ActivateMusic;
+    }
     public void Interaction()
     {
         if(PlayerPrefs.GetInt(abilityString.ToString()) == 0)
         {
-            GameManager.Instance.playerUI.MessageBoxEnable(unlockText);
             PlayerPrefs.SetInt(abilityString.ToString(), 1);
             Player.Instance.PlayerAbilityUpdate();
             Player.Instance.playerInteraction.RemoveInteraction(this);
@@ -26,11 +37,35 @@ public class UnlockAbility : MonoBehaviour, IInteractables
 
             GetComponent<CircleCollider2D>().enabled = false;
 
-            if(abilityString == GameManager.AbilityStrings.FireElement) AudioManager.Instance.StartMusicFadeOut((int)AudioManager.MusicSongs.Tutorial, true, 0.1f, 1);
-            if(abilityString == GameManager.AbilityStrings.AirElement) AudioManager.Instance.StartMusicFadeOut((int)AudioManager.MusicSongs.FireArea, true, 0.1f, 1);
-
             if (objAfterCollect != null) objAfterCollect.Deactivate();
+
+            if(dialog != null)
+            {
+                if (dialog.pauseGame == false)
+                {
+                    if (dialog.disableInputs == true)
+                    {
+                        Player.Instance.rb.linearVelocity = Vector2.zero;
+                        Player.Instance.SwitchToGround(true);
+                        Player.Instance.ChangeAnimationState("Idle");
+                        Player.Instance.state = Player.States.Ground;
+                    }
+                }
+                GameManager.Instance.playerUI.dialogBox.GetComponent<DialogBox>().DialogStart(dialog, false);
+                GameManager.Instance.playerUI.dialogBox.SetActive(true);
+            }
+            else GameManager.Instance.playerUI.MessageBoxEnable(unlockText);
+
+
             if (disableOnCollect) gameObject.SetActive(false);
+        }
+    }
+    private void ActivateMusic()
+    {
+        if (shrineEndEvent != null)
+        {
+            if (abilityString == GameManager.AbilityStrings.FireElement) AudioManager.Instance.StartMusicFadeOut((int)AudioManager.MusicSongs.Tutorial, true, 0.1f, 1);
+            if (abilityString == GameManager.AbilityStrings.AirElement) AudioManager.Instance.StartMusicFadeOut((int)AudioManager.MusicSongs.FireArea, true, 0.1f, 1);
         }
     }
 
